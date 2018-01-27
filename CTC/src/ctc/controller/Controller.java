01e97147c08c0ec700aebb55eb74039702cf8abb
Michael Plazek <plazek@Michaels-MacBook-Pro-5.local>
2018-01-27 03:35:00 -0500

    added basic UI for CTC

---
+++ CTC/src/ctc/controller/Controller.java
@@ -0,0 +1,17 @@
+package ctc.controller;
+
+import javafx.fxml.FXML;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.*;
+
+public class Controller {
+
+    @FXML
+    private void someFunction(){
+
+    }
+
+    @FXML
+    private boolean someBoolean;
+}
---
+++ CTC/src/ctc/view/UserInterface.java
@@ -0,0 +1,23 @@
+package ctc.view;
+
+import javafx.application.Application;
+import javafx.collections.ObservableArray;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ctc.fxml"));
+        primaryStage.setTitle("Centralized Traffic Control");
+        primaryStage.setScene(new Scene(root, 1100, 500));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
