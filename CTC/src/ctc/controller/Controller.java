233082fc57fd0253287048555496ad99df3b0d24
Michael Plazek <plazek@Michaels-MacBook-Pro-5.local>
2018-01-27 04:43:02 -0500

    fixed linting errors

--- CTC/src/ctc/controller/Controller.java
+++ CTC/src/ctc/controller/Controller.java
@@ -1,17 +1,14 @@
 package ctc.controller;
 
 import javafx.fxml.FXML;
-import javafx.collections.ObservableList;
-import javafx.collections.FXCollections;
-import javafx.*;
 
 public class Controller {
 
-    @FXML
-    private void someFunction(){
+  @FXML
+  private void someFunction(){
 
-    }
+  }
 
-    @FXML
-    private boolean someBoolean;
+  @FXML
+  private boolean someBoolean;
 }
--- CTC/src/ctc/view/UserInterface.java
+++ CTC/src/ctc/view/UserInterface.java
@@ -1,7 +1,6 @@
 package ctc.view;
 
 import javafx.application.Application;
-import javafx.collections.ObservableArray;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,15 +8,15 @@ import javafx.stage.Stage;
 
 public class UserInterface extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ctc.fxml"));
-        primaryStage.setTitle("Centralized Traffic Control");
-        primaryStage.setScene(new Scene(root, 1100, 500));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("ctc.fxml"));
+    primaryStage.setTitle("Centralized Traffic Control");
+    primaryStage.setScene(new Scene(root, 1100, 500));
+    primaryStage.show();
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
