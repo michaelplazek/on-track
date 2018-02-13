8ddcb4c13969214dd8dd7474e4e59ac8b396b637
Michael Plazek <plazek@Michaels-MacBook-Pro-7.local>
2018-02-13 11:33:48 -0500

    resized ui

--- CTC/src/ctc/view/Main.java
+++ CTC/src/ctc/view/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("ctc.fxml"));
     primaryStage.setTitle("Centralized Traffic Control");
-    primaryStage.setScene(new Scene(root, 1100, 500));
+    primaryStage.setScene(new Scene(root, 1400, 600));
     primaryStage.show();
   }
 
