1cb0199c873d568c96c1638962a2d8d5c97d800b
Jeremy Zang <zang.jeremy@gmail.com>
2018-02-21 17:39:27 -0500

    Added Simulation UI/Feature

--- TrainModel/src/trainmodel/Main.java
+++ TrainModel/src/trainmodel/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import trainmodel.view.TrainModelController;
 
 public class Main extends Application {
 
