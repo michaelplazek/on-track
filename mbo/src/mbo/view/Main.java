60f5b63f8f1356e5252b8f0b0ce29975dc017415
Christine Nguyễn <chrstngyn@users.noreply.github.com>
2018-02-22 18:25:27 -0500

    added exportCSV() in Controller

--- mbo/src/mbo/view/Main.java
+++ mbo/src/mbo/view/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 
 public class Main extends Application {
 
@@ -18,7 +20,7 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         launch(args);
     }
 }
