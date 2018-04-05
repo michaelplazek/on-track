f16c6312d84c213cae656d1792516e95f5112756
JonathanCepek <jonnycepek@gmail.com>
2018-04-05 18:01:59 -0400

    loop checking: added check for loops
    fixed case where block is already added.
    Currently: debugging switch to yard behavior

--- TrackController/src/trackctrl/model/TrackControllerInitializer.java
+++ TrackController/src/trackctrl/model/TrackControllerInitializer.java
@@ -11,6 +11,9 @@ import trackmodel.model.Track;
 
 public class TrackControllerInitializer {
 
+  private int lineNum = 0;
+  private TrackControllerLineManager[] lms;
+
   /**
    * Searches the Track Model instances and populates TrackControllerLineManagers
    * and TrackControllers.
@@ -19,21 +22,20 @@ public class TrackControllerInitializer {
 
     boolean ctrlrInit = true;
     HashMap<String,Track> track = Track.getListOfTracks();
-    TrackControllerLineManager[] lms = new TrackControllerLineManager[track.size()];
+    lms = new TrackControllerLineManager[track.size()];
 
     // Create TrackControllerLineManagers
-    int i = 0;
     for (Map.Entry<String, Track> trackEntry : track.entrySet()) {
       // for each String -> Track pair
 
-      lms[i] = new TrackControllerLineManager(trackEntry.getKey());
+      lms[lineNum] = new TrackControllerLineManager(trackEntry.getKey());
       int j = 0;
       Switch currSwitch;
       Block currBlock;
       currSwitch = (Switch) trackEntry.getValue().getStartBlock();
       currBlock = currSwitch;
       TrackController curr_ctrlr = new TrackController(0, currBlock.getNumber());
-
+      lms[lineNum].addController(curr_ctrlr);
       addSectionController(currBlock, curr_ctrlr, trackEntry.getValue());
 
 //          while (currSwitch.getNextBlock1() != -1 && currSwitch.getNextBlock2() != -1) {
@@ -65,7 +67,7 @@ public class TrackControllerInitializer {
 //
 //            j++;
 //          }
-          i++;
+          lineNum++;
         }
 //
 ////    lms[0] = new TrackControllerLineManager("Green");
@@ -87,14 +89,26 @@ public class TrackControllerInitializer {
 
   }
 
+
+  /** This function creates a TrackController for a particular
+   * section or multiple sections, and continues creating until
+   * a switch is Encountered.
+   * @param start - This is the first block of the track from yard
+   * @param tc - This is the TrackController we are adding the new Blocks to
+   * @param track - This is the Track object we are currently parsing
+   */
   private void addSectionController(Block start, TrackController tc, Track track) {
 
     int blocksAdded = 0;
     Block currBlock = start;
+    tc.addBlock(currBlock);
     Switch currSwitch;
 
+    //Check if first block passed in was a switch (starting case)
+    //DEBUG: May be irrelevent if passing block 63 in instead of 62 (switch)
     if (start.isSwitch()) {
-      tc.addBlock(currBlock);
+      //DEBUG: why add this?
+      //tc.addBlock(currBlock);
       currSwitch = (Switch) currBlock;
       if (currBlock.getNextBlock1() != -1) {
         //fetch next block from switch
@@ -108,6 +122,25 @@ public class TrackControllerInitializer {
       }
     }
 
+    //Check if First block to be added to controller is already maintained
+    //by a Controller.
+    //If so, break
+    //Only add controller to LM if None already contain current block
+    //TODO
+    boolean exists = false;
+    for (TrackController check : lms[lineNum].getControllersList()) {
+        if(check.hasBlock(currBlock.getNumber())) {
+          //Block already parsed, break.
+          exists = true;
+          break;
+        } else {
+        }
+    }
+
+    if (exists) {
+      lms[lineNum].addController(tc);
+    }
+
     while (true) {
       if (blocksAdded <= 16) {
         if (!(currBlock.isSwitch())) {
@@ -117,23 +150,58 @@ public class TrackControllerInitializer {
         } else {
           //Switch detected, call switch adding function
           //Create new track controller for switch
-          //DEBUG: 
-          TrackController newSwitch = new TrackController(tc.getId() + 1, currBlock.getNumber());
+          //DEBUG:
+
+          //TODO
+          //Check if TrackController for switch has already been created
+
+          exists = false;
+          for (TrackController check : lms[lineNum].getControllersList()) {
+            if(check.hasBlock(currBlock.getNumber())) {
+              //Block already parsed, break.
+              exists = true;
+              break;
+            }
+          }
+
+          //causes infinite loop
+          if (exists) {
+            return;
+          }
+
+          System.out.println("Block detected as switch: " + currBlock.getSection() + " " + currBlock.getNumber());
+          TrackController newSwitchCtrlr = new TrackController(tc.getId() + 1, currBlock.getNumber());
+          lms[lineNum].addController(newSwitchCtrlr);
           currSwitch = (Switch) currBlock;
-          addSwitchController(currSwitch, newSwitch, track);
+          addSwitchController(currSwitch, newSwitchCtrlr, track);
           break;
         }
       } else {
 
         if (!(currBlock.isSwitch())) {
           //call self with new tc instance and keep searching
-          TrackController newSegment = new TrackController(tc.getId() + 1, currBlock.getNumber());
-          addSectionController(currBlock, newSegment, track);
+
+          exists = false;
+          for (TrackController check : lms[lineNum].getControllersList()) {
+            if(check.hasBlock(currBlock.getNumber())) {
+              //Block already parsed, break.
+              exists = true;
+              break;
+            }
+          }
+
+          if (!exists) {
+            TrackController newSegmentCtrlr = new TrackController(tc.getId() + 1, currBlock.getNumber());
+            addSectionController(currBlock, newSegmentCtrlr, track);
+          } else {
+            break;
+          }
         } else {
           //Switch detected, call switch adding function
-          TrackController newSwitch = new TrackController(tc.getId() + 1, currBlock.getNumber());
+          TrackController newSwitchCtrlr = new TrackController(tc.getId() + 1, currBlock.getNumber());
+          lms[lineNum].addController(newSwitchCtrlr);
           currSwitch = (Switch) currBlock;
-          addSwitchController(currSwitch, newSwitch, track);
+          addSwitchController(currSwitch, newSwitchCtrlr, track);
           break;
         }
 
@@ -143,6 +211,12 @@ public class TrackControllerInitializer {
 
   }
 
+
+  /** This function creates a TrackController for an encountered Switch.
+   * @param create - This is the Switch object to get a TrackController
+   * @param tc - This is the TrackController to add the Switch to
+   * @param track - This is the Track object we are currently parsing
+   */
   private void addSwitchController(Switch create, TrackController tc, Track track) {
 
     //Check for ending switch
@@ -157,11 +231,18 @@ public class TrackControllerInitializer {
 
       Block seg1 = track.getBlock(create.getNextBlock1());
       Block seg2 = track.getBlock(create.getNextBlock2());
+
+      System.out.println("Seg1: " + seg1.getSection() + " " + seg1.getNumber());
+      System.out.println("Seg1.isSwitch?: " + seg1.isSwitch());
+      System.out.println("Seg2: " + seg1.getSection() + " " + seg2.getNumber());
+      System.out.println("Seg2.isSwitch?: " + seg2.isSwitch());
+
       tc.addBlock(seg1);
       tc.addBlock(seg2);
 
       TrackController seg1Ctrlr = new TrackController(tc.getId()+1, seg1.getNumber());
       TrackController seg2Ctrlr = new TrackController(tc.getId()+2, seg2.getNumber());
+
 
       addSectionController(seg1, seg1Ctrlr, track);
       addSectionController(seg2, seg2Ctrlr, track);
