cda3dec353ed9ed8215f47dea807e91d02141369
obkeel <obkeel@gmail.com>
2018-04-06 15:20:28 -0400

    changed interface to use Authority

--- TrainController/src/traincontroller/model/TrainControllerInterface.java
+++ TrainController/src/traincontroller/model/TrainControllerInterface.java
@@ -1,10 +1,12 @@
 package traincontroller.model;
 
+import utils.general.Authority;
+
 public interface TrainControllerInterface {
 
   void setBeaconSignal(Byte[] signal);
 
-  void setTrackCircuitSignal(float speed, float authority);
+  void setTrackCircuitSignal(float speed, Authority authority);
 
   void activateEmergencyBrake();
 
